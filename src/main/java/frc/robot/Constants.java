e951cefe2a65c432633cd3062f4f31ef577be6b7
Michael Grossmeyer <mikgmeyer@gmail.com>
2026-02-02 20:45:29 -0500

    Imported and refactored Tom's swerve drive code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -22,5 +26,14 @@ public final class Constants {
 
     // Deadband
     public static final double CONTROLLER_DEADBAND=0.05;
+  }
+
+  public static class SwerveDriveConstants {
+    // Swerve-specific settings (Tom's Settings)
+    public static final double SWERVE_MAX_SPEED  = 5.33; // Maximum speed in m/s
+    public static final double SWERVE_MAX_ROTATION_SPEED = 1;
+    public static final double SWERVE_ROBOT_MASS = (100) * 0.453592; // Weight in kg
+    public static final Matter SWERVE_CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), SWERVE_ROBOT_MASS);
+    public static final double SWERVE_LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag     
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,17 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ControllerConstants;
+import frc.robot.commands.SwerveFieldCentricCommand;
+import frc.robot.subsystems.SwerveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,25 +24,46 @@ import frc.robot.Constants.ControllerConstants;
  */
 public class RobotContainer {
 
-  // Controllers
-  private final CommandXboxController driverController = new CommandXboxController(ControllerConstants.CONTROLLER_DRIVER_PORT);
+  	// Initialize Subsystems
+  	private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    this.configureBindings();
-  }
+  	// Get controllers
+  	private final CommandXboxController controllerDriver = new CommandXboxController(ControllerConstants.CONTROLLER_DRIVER_PORT);
+  	// private final CommandXboxController controllerOperator = new CommandXboxController(ControllerConstants.CONTROLLER_OPERATOR_PORT);
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
+	public RobotContainer() {
+		this.configureBindings(); // May want to split this into two different methods - one controller + two controllers
 
-  }
+		this.configureDefaultDriveCommand();
+	}
+
+	private void configureDefaultDriveCommand() {
+		boolean isOpenField = false;
+		SwerveFieldCentricCommand fieldCentricDrive = new SwerveFieldCentricCommand(
+			this.swerveSubsystem,
+			() -> MathUtil.applyDeadband(controllerDriver.getLeftX(), ControllerConstants.CONTROLLER_DEADBAND),
+			() -> MathUtil.applyDeadband(controllerDriver.getLeftY(), ControllerConstants.CONTROLLER_DEADBAND),
+			() -> controllerDriver.getRightX(), // this used to be controllerDriver.getRawAxis(2), leaving this note here in case of bugs
+			isOpenField
+		);
+
+		this.swerveSubsystem.setDefaultCommand(fieldCentricDrive);     
+	}
+
+	/**
+	 * Use this method to define your trigger->command mappings. Triggers can be created via the
+	 * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+	 * predicate, or via the named factories in {@link
+	 * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+	 * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+	 * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+	 * joysticks}.
+	 */
+	private void configureBindings() {
+
+	}
+
+	public Command getAutonomousCommand() {
+		return Commands.print("No autonomous command configured");
+	}
 }
