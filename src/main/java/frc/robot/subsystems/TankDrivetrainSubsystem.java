e68b2e415a577e33b6047a68b5601296a2ab98aa
Michael Grossmeyer <mikgmeyer@gmail.com>
2026-02-25 17:19:02 -0500

    Swap out the PWMSparkMax objects for the REVLib SparkMax objects

--- src/main/java/frc/robot/subsystems/TankDrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDrivetrainSubsystem.java
@@ -1,18 +1,25 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.math.filter.SlewRateLimiter;
 // import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TankDriveConstants;
 
 public class TankDrivetrainSubsystem extends SubsystemBase {
 
-	private final PWMSparkMax motorLeftFront = new PWMSparkMax(TankDriveConstants.MOTOR_LEFT_FRONT_ID);
-	private final PWMSparkMax motorLeftBack = new PWMSparkMax(TankDriveConstants.MOTOR_LEFT_BACK_ID);
-	private final PWMSparkMax motorRightFront = new PWMSparkMax(TankDriveConstants.MOTOR_RIGHT_FRONT_ID);
-	private final PWMSparkMax motorRightBack = new PWMSparkMax(TankDriveConstants.MOTOR_RIGHT_BACK_ID);
+	private final SparkMax leftLeader = new SparkMax(TankDriveConstants.MOTOR_LEFT_FRONT_ID, MotorType.kBrushed);
+	private final SparkMax leftFollower = new SparkMax(TankDriveConstants.MOTOR_LEFT_BACK_ID, MotorType.kBrushed);
+	private final SparkMax rightLeader = new SparkMax(TankDriveConstants.MOTOR_RIGHT_FRONT_ID, MotorType.kBrushed);
+	private final SparkMax rightFollower = new SparkMax(TankDriveConstants.MOTOR_RIGHT_BACK_ID, MotorType.kBrushed);
+
 
 	// Handles easing for streams of inputs
 	private final SlewRateLimiter leftSlewFilter = new SlewRateLimiter(TankDriveConstants.SLEW_RATE);
@@ -20,18 +27,43 @@ public class TankDrivetrainSubsystem extends SubsystemBase {
 
 	private final DifferentialDrive tankDrive;
 
-  public TankDrivetrainSubsystem() {
+  	public TankDrivetrainSubsystem() {
 
-		// Tie motors together, follower motors just copy the leader motor verbatim
-		this.motorLeftFront.addFollower(motorLeftBack);
-		this.motorRightFront.addFollower(motorRightBack);
+		SparkMaxConfig globalConfig = new SparkMaxConfig();
+		SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+		SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
+		SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
+	
+		/*
+		* Set parameters that will apply to all SPARKs. We will also use this as
+		* the left leader config.
+		*/
+		globalConfig
+			.smartCurrentLimit(50)
+			.idleMode(IdleMode.kBrake);
 
-		// Since the back motors have been added as followers, we only need to 
-		// interact with the front motors (leader motors). This includes inverting.
-		this.motorLeftFront.setInverted(TankDriveConstants.ARE_MOTORS_LEFT_INVERTED);
-		this.motorRightFront.setInverted(TankDriveConstants.ARE_MOTORS_RIGHT_INVERTED);
+		// Apply the global config and invert since it is on the opposite side
+		rightLeaderConfig
+			.apply(globalConfig)
+			.inverted(true);
 
-		this.tankDrive = new DifferentialDrive(motorLeftFront, motorRightFront);
+		// Apply the global config and set the leader SPARK for follower mode
+		leftFollowerConfig
+			.apply(globalConfig)
+			.follow(leftLeader);
+
+		// Apply the global config and set the leader SPARK for follower mode
+		rightFollowerConfig
+			.apply(globalConfig)
+			.follow(rightLeader);
+	
+
+		leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+		leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+		rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+		rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+		this.tankDrive = new DifferentialDrive(leftLeader, rightLeader);
 	}
 
   public void tankDrive(double leftSpeed, double rightSpeed) {
